b307f3898d77340d0fc16102eb458dd659f80cf9
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2015-11-04 23:10:53 +0100

    Convert style.css to *.scss + style.css and start npm project

--- functions.php
+++ functions.php
@@ -1,13 +1,9 @@
 <?php
 
 function twentyfourlimacs_enqueue_styles() {
-
-    $parent_style = 'parent-style';
-
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style )
-    );
+    wp_enqueue_style( 'parent-style', get_template_directory_uri()   . '/style.css' );
+    wp_enqueue_style( 'child-default-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
+    wp_enqueue_style( 'child-main-style',    get_stylesheet_directory_uri() . '/main.css', array('child-default-style'));
 }
+
 add_action( 'wp_enqueue_scripts', 'twentyfourlimacs_enqueue_styles' );
